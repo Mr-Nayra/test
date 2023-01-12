019f387b1951a63621f512658e793918310665c5
Rajveer jatav <87698946+Rajveerjatav@users.noreply.github.com>
2023-01-12 14:20:32 +0530

    top header create

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { Fragment } from "react";
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import Navbar from "./container/header/Navbar";
+import TopHeader from "./container/header/TopHeader";
 import { router } from "./router";
 
 function App() {
   return (
     <Fragment>
+      <TopHeader />
       <Navbar />
       <RouterProvider router={router} />
     </Fragment>
