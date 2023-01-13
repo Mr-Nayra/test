3dec4d1197ecc1cd8d3b3a6694375c927ef4aae4
Rajveer jatav <87698946+Rajveerjatav@users.noreply.github.com>
2023-01-13 19:31:35 +0530

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import AboutSection from "./components/about/AboutSection";
 import BlogHome from "./components/blog/BlogHome";
 import HeroBanner from "./components/heroBanner/HeroBanner";
 import Testimonials from "./components/testimonials/Testimonials";
@@ -7,6 +8,7 @@ const Home = () => {
   return (
     <Fragment>
       <HeroBanner />
+      <AboutSection />
       <Testimonials />
       <BlogHome />
     </Fragment>
