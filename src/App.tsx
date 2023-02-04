04f44d077e8cd61efb95ef54ab5335924b6f1176
Rajveer jatav <87698946+Rajveerjatav@users.noreply.github.com>
2023-02-04 14:46:14 +0530

    remove old blog

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,6 @@ import Navbar from "./container/header/Navbar";
 import About from "./pages/about";
 import Home from "./pages/home";
 import TopHeader from "./container/header/TopHeader";
-// import CareRetraining from "./pages/blog-page/CareRetraining";
-// import BiasSystems from "./pages/blog-page/BiasSystems";
-// import DataDrift from "./pages/blog-page/DataDrift ";
-// import BiasRecommenderSystems from "./pages/blog/BiasRecommenderSystems";
-// import AboutRetraining from "./pages/blog/AboutRetraining";
-// import DataDriftDetection from "./pages/blog/DataDriftDetection";
 import BlogPage from "./pages/blog";
 
 function App() {
