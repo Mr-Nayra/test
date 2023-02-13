6d7da74400f811010c59714b24f43ac2b13c7261
ajay2966 <ajmadhpuriya1234@gmail.com>
2023-02-13 14:50:06 +0530

    update

--- src/pages/blog/blog-page/BlogCards.tsx
+++ src/pages/blog/blog-page/BlogCards.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useState } from "react";
+import React, { memo, useMemo, useState } from "react";
 import classes from "./blogRoot.module.scss";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { Link } from "react-router-dom";
@@ -7,7 +7,17 @@ import CustomButton from "../../../components/button/CustomButton";
 
 const BlogCards = () => {
   const [data, setData] = useState(5);
-  const dataSlice = BlogHomeCarouselData.slice(0, data);
+  const blogData = useMemo(() => {
+    let newData: any[] = [];
+    BlogHomeCarouselData.forEach((item, index) => {
+      if (index !== 0) {
+        newData.push(item);
+      }
+    });
+    return newData;
+  }, [BlogHomeCarouselData]);
+
+  const dataSlice = blogData.slice(0, data);
 
   return (
     <div className={classes.cardRoot}>
@@ -15,26 +25,28 @@ const BlogCards = () => {
         More Reads
       </h2>
       <div className={classes.cardRow}>
-        {dataSlice.map((item, index) => (
-          <div key={`${"_" + index}`} className={classes.cardCol}>
-            <div className={classes.card}>
-              <img className={classes.cardImg} src={item.cardImg} alt="" />
-              <div className={classes.textSection}>
-                <p className={classes.author}> {item.author} </p>
-                <h3 className={classes.cardHeading}>{item.heading}</h3>
-                <p className={classes.cardDescription}>{item.description}</p>
-                <Link
-                  to={`/blog/${item.id}`}
-                  target={"_blank"}
-                  className={classes.cardReadMore}
-                >
-                  <span> Read More</span>
-                  <AiOutlineArrowRight size={14} />
-                </Link>
+        {dataSlice.map((item, index) => {
+          return (
+            <div key={`${"_" + index}`} className={classes.cardCol}>
+              <div className={classes.card}>
+                <img className={classes.cardImg} src={item.cardImg} alt="" />
+                <div className={classes.textSection}>
+                  <p className={classes.author}> {item.author} </p>
+                  <h3 className={classes.cardHeading}>{item.heading}</h3>
+                  <p className={classes.cardDescription}>{item.description}</p>
+                  <Link
+                    to={`/blog/${item.id}`}
+                    target={"_blank"}
+                    className={classes.cardReadMore}
+                  >
+                    <span> Read More</span>
+                    <AiOutlineArrowRight size={14} />
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
       <div className={classes.cardBtnSection}>
         <CustomButton
@@ -49,7 +61,7 @@ const BlogCards = () => {
             setData(data + data);
           }}
           disabled={
-            BlogHomeCarouselData.length === dataSlice.length ? true : false
+            BlogHomeCarouselData.length - 1 === dataSlice.length ? true : false
           }
         />
       </div>
