726f26e478fc9b7d994ccc51d84ccfc1d28532fd
Rajveer jatav <87698946+Rajveerjatav@users.noreply.github.com>
2023-03-07 15:05:31 +0530

    order change Testimonials and spacing change in blog pages

--- src/utils/TestimonialsCarouseldata.ts
+++ src/utils/TestimonialsCarouseldata.ts
@@ -7,25 +7,18 @@ import andre_franca from "./images/testimonial/andre_franca.webp";
 
 export const TestimonialsCarouselData = [
   {
+    userName: "Andre Franca",
+    userPosition: "VP of R&D @ causaLens",
+    userProfile: andre_franca,
+    description:
+      "I have been looking for a good open-source observability solution for monitoring ML models for a while now and couldn't have been happier to have found UpTrain! Really excited to see what UpTrain team is building and closely following their progress.",
+  },
+  {
     userName: "David Chang",
     userPosition: "ML Engineer, Docugami",
     userProfile: david_chang,
     description:
       "Model observability is a real pain point for data scientists, and a tool like UpTrain can help identify model failures and their potential causes in order to combat performance degradation ASAP",
-  },
-  {
-    userName: "Harsh Gupta",
-    userPosition: "PhD, University of Illinois at Urbana-Champaign",
-    userProfile: harsh_gupta,
-    description:
-      "Adding custom monitors is a critical requirement for any machine learning observability toolkit, and it's great to see that the team behind UpTrain is designing the project with ease of customization at the core of it.",
-  },
-  {
-    userName: "Koulik Khamaru",
-    userPosition: "Assistant Professor of Statistics at Rutgers University",
-    userProfile: koulik_khamaru,
-    description:
-      "It's great to see an ML observability tool like UpTrain who is combining statistics, deep learning methods, and customization in the right way to create a comprehensive solution that actually works!",
   },
   {
     userName: "Ayush Jasuja",
@@ -42,10 +35,17 @@ export const TestimonialsCarouselData = [
       "Monitoring your models in production is as important as building them in the first place. The AI community lacks good monitoring solutions which allows deep customizations and it's great to see an open-source project like UpTrain solving it head-on.",
   },
   {
-    userName: "Andre Franca",
-    userPosition: "VP of R&D @ causaLens",
-    userProfile: andre_franca,
+    userName: "Harsh Gupta",
+    userPosition: "PhD, University of Illinois at Urbana-Champaign",
+    userProfile: harsh_gupta,
     description:
-      "I have been looking for a good open-source observability solution for monitoring ML models for a while now and couldn't have been happier to have found UpTrain! Really excited to see what UpTrain team is building and closely following their progress.",
+      "Adding custom monitors is a critical requirement for any machine learning observability toolkit, and it's great to see that the team behind UpTrain is designing the project with ease of customization at the core of it.",
+  },
+  {
+    userName: "Koulik Khamaru",
+    userPosition: "Assistant Professor of Statistics at Rutgers University",
+    userProfile: koulik_khamaru,
+    description:
+      "It's great to see an ML observability tool like UpTrain who is combining statistics, deep learning methods, and customization in the right way to create a comprehensive solution that actually works!",
   },
 ];
