68dc04d8e04fa662d8955c5678d79ab7159c3a2b
Dominastorm <dchawla228@gmail.com>
2023-08-01 13:19:13 +0530

    feat: Add ChatBot component

---
+++ src/components/chat-bot/ChatBot.tsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import MessageInput from './MessageInput';
+import classes from './index.module.scss';
+
+const ChatBot: React.FC = () => {
+  const [messages, setMessages] = useState<{ role: string; content: string }[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+  const [selectedDocument, setSelectedDocument] = useState<string>(''); // State for selected document
+
+  // Handle user input and call the API for the chatbot response
+  const handleUserInput = async () => {
+    if (inputValue.trim() !== '') {
+      // Include selected document in the request body
+      const requestBody = {
+        question: inputValue,
+        document: selectedDocument,
+      };
+      console.log(messages)
+      const newMessage = {
+        role: 'user',
+        content: inputValue,
+      };
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+
+      try {
+        const response = await fetch('http://localhost:9000/question', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(requestBody),
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to get response from the chatbot API.');
+        }
+
+        const data = await response.json();
+
+        const chatbotResponse = data.answer;
+        const newChatbotMessage = {
+          role: 'chatbot',
+          content: `ChatBot: ${chatbotResponse}`,
+        };
+        setMessages((prevMessages) => [...prevMessages, newChatbotMessage]);
+      } catch (error) {
+        console.error('Error fetching chatbot response:', error);
+      }
+
+      setInputValue('');
+    }
+  };
+
+  // Handle Enter key press in the input field
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleUserInput();
+    }
+  };
+
+  useEffect(() => {
+    // Scroll to the bottom of the chat window whenever new messages are added
+    const chatWindow = document.querySelector('.chat-window');
+    if (chatWindow) {
+      chatWindow.scrollTop = chatWindow.scrollHeight;
+    }
+  }, [messages]);
+
+  // List of documents in the dropdown menu
+  const documentOptions = ["LangChain", "LlamaIndex", "UpTrain"];
+
+  return (
+    <div className={classes.chatbot}>
+      <div className={classes.dropdownContainer}>
+        <select
+          value={selectedDocument}
+          onChange={(e) => setSelectedDocument(e.target.value)}
+        >
+          <option value="">Choose Documentation</option>
+          {documentOptions.map((document, index) => (
+            <option key={index} value={document}>
+              {document}
+            </option>
+          ))}
+        </select>
+      </div>
+      <MessageInput
+        placeholder="Type a message..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onSendButtonClick={handleUserInput}
+        onKeyPress={handleKeyPress}
+      />
+      <div className={classes.chatWindow}>
+        <ul>
+          {messages.map((message, index) => (
+            <li key={index} className={message.role}>
+              {message.content}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBot;
---
+++ src/components/chat-bot/MessageInput.tsx
@@ -0,0 +1,78 @@
+// src/MessageInput.tsx
+import React, { useState, useRef } from 'react';
+import styled from '@emotion/styled';
+import classes from './index.module.scss';
+
+// Styled components for the Message Input
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  border-top: 1px solid #ccc;
+`;
+
+const InputField = styled.input`
+  flex: 1;
+  margin-right: 8px;
+  padding: 8px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+
+const SendButton = styled.button`
+  padding: 8px 16px;
+  font-size: 14px;
+  background-color: #6d69d2;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+interface MessageInputProps {
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onSendButtonClick: () => void;
+  onKeyPress: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+}
+
+const MessageInput: React.FC<MessageInputProps> = ({
+  placeholder,
+  value,
+  onChange,
+  onSendButtonClick,
+  onKeyPress,
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // Handle the focus on the input field when the user clicks the send button
+  const handleSendButtonClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+    onSendButtonClick();
+  };
+
+  return (
+    <InputContainer>
+      <InputField
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+        ref={inputRef}
+      />
+      <SendButton onClick={handleSendButtonClick}>Ask</SendButton>
+    </InputContainer>
+  );
+};
+
+export default MessageInput;
