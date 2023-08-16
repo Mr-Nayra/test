27b29fdb04e4d2c69d9f40707b91fc3da0601026
Dominastorm <dchawla228@gmail.com>
2023-08-16 13:36:05 +0530

    Add Posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,15 +7,25 @@ import TheFooter from "@/components/footer/TheFooter";
 import "../styles/prism-darcula.css";
 import CookiesBanner from "@/components/cookies-banner/CookiesBanner";
 import { MantineProvider } from "@mantine/core";
+import { PostHogProvider } from "posthog-js/react";
+import posthog from "posthog-js";
+
+if (typeof window !== "undefined") {
+  posthog.init("", {
+    api_host: "https://app.posthog.com",
+  });
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider>
-      <TopHeader />
-      <Header />
-      <Component {...pageProps} />
-      <TheFooter />
-      {/* <CookiesBanner /> */}
-    </MantineProvider>
+    <PostHogProvider client={posthog}>
+      <MantineProvider>
+        <TopHeader />
+        <Header />
+        <Component {...pageProps} />
+        <TheFooter />
+        {/* <CookiesBanner /> */}
+      </MantineProvider>
+    </PostHogProvider>
   );
 }
