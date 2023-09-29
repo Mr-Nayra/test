92a4cc3cc4ebd4f3bf2abbeeaf34cce2e43496b8
Mr.Nayra <aryanedu5786@gmail.com>
2023-09-29 20:31:28 +0530

    removed mobile code from hero

--- src/pages/index-components/hero-banner/HeroBanner.tsx
+++ src/pages/index-components/hero-banner/HeroBanner.tsx
@@ -15,67 +15,32 @@ const HeroBanner = () => {
 
   return (
     <>
-      {width > 1033 ? (
-        <div className={classes.root}>
-          <div className={classes.textOverlay}>
-            <div style={{ width: "100%" }}>
-              <div className={classes.row}>
-                <div className={`${classes.col} ${classes.textCol}`}>
-                  <HeroBannerText />
-                  <PoweredBy />
-                </div>
-                <div className={`${classes.col}`}>
-                  <video
-                    width="100%"
-                    height="100%"
-                    autoPlay={true}
-                    disablePictureInPicture
-                    muted
-                    loop
-                    playsInline
-                    title="Demo of How UpTrain evaluation works?"
-                  >
-                    <source
-                      // src="https://uptrain-assets.s3.ap-south-1.amazonaws.com/videos/backgroundVideo.mp4"
-                      src="./heroVideo.webm"
-                      type="video/mp4"
-                    />
-                    Your browser does not support the video tag.
-                  </video>
-                </div>
-              </div>
+      <div className={classes.root}>
+        <div style={{ width: "100%" }}>
+          <div className={classes.row}>
+            <div className={`${classes.col} ${classes.textCol}`}>
+              <HeroBannerText />
+              {width > 1033 && <PoweredBy />}
+            </div>
+            <div className={`${classes.col}`}>
+              <video
+                width="100%"
+                height="100%"
+                autoPlay={true}
+                disablePictureInPicture
+                muted
+                loop
+                playsInline
+                title="Demo of How UpTrain evaluation works?"
+              >
+                <source src="./heroVideo.webm" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
             </div>
           </div>
+          {width < 1033 && <PoweredBy />}
         </div>
-      ) : (
-        <div className={classes.root}>
-          <div style={{ width: "100%" }}>
-            <div className={classes.row}>
-              <div className={`${classes.col} ${classes.textCol}`}>
-                <HeroBannerText />
-              </div>
-              <div className={classes.col}>
-                <div className={classes.mobileVideoContainer}>
-                  <video
-                    width="100%"
-                    height="100%"
-                    autoPlay={true}
-                    disablePictureInPicture
-                    muted
-                    loop
-                    playsInline
-                    title="Demo of How UpTrain evaluation works?"
-                  >
-                    <source src="./heroVideo.webm" type="video/mp4" />
-                    Your browser does not support the video tag.
-                  </video>
-                </div>
-              </div>
-            </div>
-            <PoweredBy />
-          </div>
-        </div>
-      )}
+      </div>
       <Line />
       {/* <AcceptModal opened={opened} close={close} /> */}
     </>
